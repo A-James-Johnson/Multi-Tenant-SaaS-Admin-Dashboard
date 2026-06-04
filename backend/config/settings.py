8d09db604b1892b9022c66e1d8fee675435f4d0b
A-James-Johnson <jamesjohnson.a2022it@sece.ac.in>
2026-06-04 13:02:48 +0530

    updates code

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -7,8 +7,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('DJANGO_SECRET_KEY', default='django-insecure-dev-key-change-me')
 DEBUG = config('DJANGO_DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
-
+# ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = [
+    'multi-tenant-saas-admin-dashboard.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
